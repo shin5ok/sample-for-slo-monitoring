a617c077d8978a618d6b140f6f2c05f2bcc10661
shin5ok <shin5ok@55mp.com>
2022-10-08 18:04:32 +0900

    fix the env

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gcloud run deploy --region=asia-northeast1 --source=. --set-env-vars=PROJECT=$PROJECT --allow-unauthenticated my-app
+gcloud run deploy --region=asia-northeast1 --source=. --set-env-vars=PROJECT_ID=$PROJECT_ID --allow-unauthenticated my-app
